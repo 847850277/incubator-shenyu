bdd31d06dbd3244d16e525ba5bf436af0a70f525
zhengpeng <847850277@qq.com>
2022-08-31 11:52:54 +0800

    feature: 3806 temp commit

---
+++ shenyu-client-agent/shenyu-client-agent-dubbo/src/main/java/org/apache/shenyu/client/agent/dubbo/DubboAgent.java
@@ -0,0 +1,18 @@
+package org.apache.shenyu.client.agent.dubbo;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @author zhengpeng
+ * @date 2022/8/30 2:12 下午
+ **/
+public class DubboAgent {
+
+
+    public static void premain(String arg, Instrumentation instrumentation) {
+
+
+
+    }
+
+}
---
+++ shenyu-client-agent/shenyu-client-agent-dubbo/src/main/java/org/apache/shenyu/client/agent/dubbo/beanlistener/AlibabaDubboBeanListener.java
@@ -0,0 +1,11 @@
+package org.apache.shenyu.client.agent.dubbo.beanlistener;
+
+/**
+ * @author zhengpeng
+ * @date 2022/8/30 2:19 下午
+ **/
+public class AlibabaDubboBeanListener {
+
+
+
+}
